ea427f56035573a53d9514e9a933516971c2bdbb
tonygiang <tony_s_giang@yahoo.com>
2022-08-04 18:35:39 +0700

    Version 1.1
    - Reduce comment redundancy.

--- Object.Is.cs
+++ Object.Is.cs
@@ -15,18 +15,12 @@ namespace CLSS
     public static bool Is<R>(this object source) where R : class
     { return source is R; }
 
-    /// <summary>
-    /// Returns whether this is the specified reference type. This method has a
-    /// boxing overhead.
-    /// </summary>
-    /// <typeparam name="R">The reference type to check against.</typeparam>
-    /// <param name="source">The object to test type.</param>
+    /// <inheritdoc cref="Is{R}(object)"/>
     /// <param name="castedValue">When this method returns, contains the result
     /// of <paramref name="source"/> casted to the specified reference type, if
     /// the type check results in True, or null if type check results in False.
     /// This parameter is passed uninitialized; any value originally supplied in
     /// <paramref name="castedValue"/> will be overwritten.</param>
-    /// <returns>Whether the source is the specified reference type.</returns>
     public static bool Is<R>(this object source, out R castedValue)
       where R : class
     {
